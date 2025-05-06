9b769754851fe7bc170baa6aa0e47315740d988b
Frank Yang <puilp0502@gmail.com>
2025-05-07 02:39:16 +0900

    chore: fix error page

--- client/src/components/EditorErrorBoundary.tsx
+++ client/src/components/EditorErrorBoundary.tsx
@@ -41,10 +41,9 @@ export function EditorErrorBoundary() {
                     <div className="flex flex-col space-y-2">
                         <button
                             className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
-                            onClick={() =>
-                                navigate("/editor/wumpus-touch-green-grass")}
+                            onClick={() => navigate("/")}
                         >
-                            Go to Default Pack
+                            메인 페이지로
                         </button>
                         <button
                             className="px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300 transition-colors"
